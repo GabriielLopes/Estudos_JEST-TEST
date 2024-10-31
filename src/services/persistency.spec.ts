3e0a7afa005b5f88cf64a76c2629580c67edc9d4
Gabriel Lopes <Gabrielcostaplay@gmail.com>
2024-10-31 12:55:10 -0300

    Aula01 - instação e introdução ao JEST

---
+++ src/services/persistency.spec.ts
@@ -0,0 +1,10 @@
+describe('TESTANDO ALGO', () => {
+  it('Descrição do teste (IT)', () => {
+    const number = 1;
+    expect(number).not.toBe(1);
+  });
+  test('Descrição do teste (TEST)', () => {
+    const name = 'Gabriel';
+    expect(name).toBe('Gabriel');
+  });
+});
